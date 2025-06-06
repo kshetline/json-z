9c178ad1d11c43bf23077069e14076df25d3f1ce
kshetline <kerry@shetline.com>
2025-06-05 22:24:59 -0400

    Update rollup.config.js.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ const commonjs = require('rollup-plugin-commonjs');
 const terser = require('@rollup/plugin-terser');
 // noinspection JSUnresolvedReference
 const dts = require('rollup-plugin-dts').dts;
-const shebang = require('rollup-plugin-preserve-shebang');
-const json = require('@rollup/plugin-json');
 const pkg = require('./package.json');
 
 module.exports = [
@@ -61,20 +59,5 @@ module.exports = [
     plugins: [
       dts()
     ]
-  },
-  // CLI
-  {
-    input: 'lib/cli.js',
-    output: {
-      file: pkg.bin['json-z'],
-      format: 'cjs'
-    },
-    plugins: [
-      json(),
-      shebang(),
-      resolve(),
-      commonjs()
-    ],
-    external: ['fs', 'path']
   }
 ];
